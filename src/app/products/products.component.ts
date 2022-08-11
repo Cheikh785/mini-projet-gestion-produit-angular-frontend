68212c976280159f0b4f50eed544117cfbb1ca2f
Cheikh785 <cheikhomar.cof@gmail.com>
2022-08-11 22:46:52 +0000

    styling product component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/Models/product.model';
+import { ProductsService } from 'src/services/products.service';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+	selector: 'app-products',
+	templateUrl: './products.component.html',
+	styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+	listProducts: any = [];
 
-  ngOnInit(): void {
-  }
+	constructor(private productService: ProductsService) { }
+
+	ngOnInit() {
+		this.productService.getAllProducts()
+			.subscribe({
+				next: (data: Product) => {
+					this.listProducts = data;
+					console.log(this.listProducts);
+				},
+				error: (error) => {
+					console.log(error);
+				}
+			});
+  	}
 
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from 'src/Models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  	providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+	apiUrl = "http://localhost:3000/products";
+
+	constructor(private http: HttpClient) { }
+
+	getAllProducts() {
+		return this.http.get<Product>(this.apiUrl);
+	}
 }
